fed6267d62ca83a40f71e70d58c2fe801c6ab1e1
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-11-18 19:09:02 +0700

    Update TugasPert1.m

--- TugasPert1.m
+++ TugasPert1.m
@@ -1,14 +1,13 @@
+clear;
 n = 6;
 m = zeros(n,n);
 for x=1:n,
     for y=1:n
         if x==y
             m(x,y) = x;
-        end
-        if x==y+2
+        elseif x==y+2
             m(x,y) = 2;
-        end
-        if x+2==y
+        elseif x+2==y
             m(x,y) = -1;
         end
     end
