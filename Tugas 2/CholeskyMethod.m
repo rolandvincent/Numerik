18bb8adf805f86c7605ec32ba8fc19ef764b25b9
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-11-27 11:35:34 +0700

    Update CholeskyMethod.m

--- Tugas 2/CholeskyMethod.m
+++ Tugas 2/CholeskyMethod.m
@@ -6,7 +6,6 @@ function x = CholeskyMethod(A,b)
     for i = 2:n,
         y(i) = (b(i)-L(i,1:i-1)*y(1:i-1))/L(i,i);
     end
-    % Solve the upper triangular system L'x = y (back substitution)
     x = zeros(n,1);
     x(n) = y(n)/U(n,n);
     for i = n-1:-1:1,
