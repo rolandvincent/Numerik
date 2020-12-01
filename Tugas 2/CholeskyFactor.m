38fdc1d38a709871d267b92ec432986d56c5ac5b
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-12-01 21:53:25 +0700

    t

--- Tugas 2/CholeskyFactor.m
+++ Tugas 2/CholeskyFactor.m
@@ -1,6 +1,8 @@
 function [L, U] = CholeskyFactor(A)
     n = length(A);
+    % n = 3
     L = zeros(n);
+    % L = [0,0,0;0,0,0;0,0,0]
     for i=1:n
         L(i,i) = sqrt(A(i,i)-L(i,1:i-1)*L(i,1:i-1)');
         for j=i+1:n
