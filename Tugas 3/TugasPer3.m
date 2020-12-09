6203fe3ad7c3d0457d731986367fc7b53436c53f
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-12-09 11:54:14 +0700

    Numerik

--- Tugas 3/TugasPer3.m
+++ Tugas 3/TugasPer3.m
@@ -1,4 +1,4 @@
 F = sym('exp(-x/2)+log(x+2)-2');
 [r,n] = Secant_Function(F,0,1,1e-6,20);
-disp(r);
-disp(n);
+fprintf('r=%f\n',r); % Print float, r adalah salah satu akar dari persamaan
+fprintf('r=%d\n',n); % Print integer, n adalah jumlah loop sampai mendapatkan hasil r
