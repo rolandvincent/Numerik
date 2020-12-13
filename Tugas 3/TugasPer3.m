2a76ec87fc3e9583aa75abfad0598e91b89cac2a
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-12-13 20:08:01 +0700

    Metnum

--- Tugas 3/TugasPer3.m
+++ Tugas 3/TugasPer3.m
@@ -1,5 +1,6 @@
 F = sym('exp(-x/2)+log(x+2)-2');
 [r,n] = Secant_Function(F,0,1,1e-6,20);
+disp('maka,');
 disp('f(x)=exp(-x/2)+log(x+2)-2')
 fprintf('r=%f\n',r); % Print float, r adalah salah satu akar dari persamaan
 fprintf('n=%d\n',n); % Print integer, n adalah jumlah loop sampai mendapatkan hasil r
