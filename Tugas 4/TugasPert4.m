81a2f4e300e6967344013db341463094d1f1a7cf
Roland Vincent <52077393+rolandvincent@users.noreply.github.com>
2020-12-15 20:35:01 +0700

    Update TugasPert4.m

--- Tugas 4/TugasPert4.m
+++ Tugas 4/TugasPert4.m
@@ -15,8 +15,8 @@ disp(result);
 disp('Dengan error Left dan Right End Point sekitar');
 % menghitung error
 K2=matlabFunction(diff(f)); % mencari turunan pertama.
-fa = K2(a); % memasukkan nilai a ke turunan kedua fungsi
-fb = K2(b); % memasukkan nilai b ke turunan kedua fungsi
+fa = K2(a); % memasukkan nilai a ke turunan pertama fungsi
+fb = K2(b); % memasukkan nilai b ke turunan pertama fungsi
 if fa>fb % mencari yang terbesar
     ft=fa;
 else
